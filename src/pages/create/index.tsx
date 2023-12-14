86c3eab4677ff318af5ccda4a68f6ebc38ec9b8a
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-12-14 16:45:46 +0530

    add create pego

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -16,45 +16,34 @@ export default function Create() {
             <p className="text-2xl font-bold text-[#9c9e9e]">
               Log in your wallet to craft CraftNFTs. 🪄
             </p>
-            <div className="grid grid-cols-3 gap-3 mt-10">
+            <div className="grid grid-cols-2 gap-3 mt-10">
               <Link
-                href={`/create/fuji`}
+                href={`/create/pego`}
                 className="border-[2px] border-[#3c3f41] flex flex-col justify-center items-center h-[300px] rounded-2xl"
               >
                 <Image
-                  src="/tech/avalanche.png"
+                  src="/tech/pego.png"
                   width={100}
                   height={100}
-                  alt="mumbai"
+                  alt="pego"
                 />
                 <p className="text-white font-bold text-2xl mt-4">
-                  Avalanche Fuji
+                  PEGO Mainnet
                 </p>
               </Link>
-              <Link
-                href={`/create/sepolia`}
-                className="border-[2px] border-[#3c3f41] flex flex-col justify-center items-center h-[300px] rounded-2xl"
-              >
-                <Image
-                  src="/tech/blue-ethereum.png"
-                  width={100}
-                  height={100}
-                  alt="sepolia"
-                />
-                <p className="text-white font-bold text-2xl mt-4">Sepolia</p>
-              </Link>
-              <Link
-                href={`/create/mumbai`}
-                className="border-[2px] border-[#3c3f41] flex flex-col justify-center items-center h-[300px] rounded-2xl"
-              >
+
+              <div className="border-[2px] border-[#3c3f41] flex flex-col justify-center items-center h-[300px] rounded-2xl cursor-default">
                 <Image
                   src="/tech/polygon.png"
                   width={100}
                   height={100}
                   alt="mumbai"
                 />
-                <p className="text-white font-bold text-2xl mt-4">Mumbai</p>
-              </Link>
+                <p className="text-[#9c9e9e] font-bold text-2xl mt-4">
+                  Other Blockchains
+                </p>
+                <p className="text-[#3c3f41] font-semibold">Coming soon...</p>
+              </div>
             </div>
           </div>
         </div>
